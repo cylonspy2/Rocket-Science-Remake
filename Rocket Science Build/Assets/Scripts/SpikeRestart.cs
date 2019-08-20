e3a6464b25d8ac1de40bb610be1a3c4881705740
Rowan Brown <rowanbrown644@gmail.com>
2019-08-20 13:53:05 -0400

    added incomplete spike script

---
+++ Rocket Science Build/Assets/Scripts/SpikeRestart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeRestart : MonoBehaviour
+{
+	public GameObject player;
+	private Scene corn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		corn = SceneManager.GetActiveScene();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.gameObject.name == player.name)
+		{
+			SceneManager.LoadScene(corn.name);
+		}
+	}
+}
